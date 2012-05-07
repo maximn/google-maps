bd315c84cf1fba2cd2ef43a902eec74547deb295
maximnovak <maxim.novak@gmail.com>
2012-05-07 12:11:43 +0300

    Fixed as sugested -

--- GoogleMapsApi/Engine/MapsAPIGenericEngine.cs
+++ GoogleMapsApi/Engine/MapsAPIGenericEngine.cs
@@ -20,108 +20,93 @@ namespace GoogleMapsApi.Engine
 			}
 		}
 
-  protected IAsyncResult BeginQueryGoogleAPI(TRequest request, AsyncCallback asyncCallback, object state)
-   {
-     // Must use TaskCompletionSource because in .NET 4.0 there's no overload of ContinueWith that accepts a state object (used in IAsyncResult).
-     // Such overloads have been added in .NET 4.5, so this can be removed if/when the project is promoted to that version.
-     // An example of such an added overload can be found at: http://msdn.microsoft.com/en-us/library/hh160386.aspx
+		protected IAsyncResult BeginQueryGoogleAPI(TRequest request, AsyncCallback asyncCallback, object state)
+		{
+			// Must use TaskCompletionSource because in .NET 4.0 there's no overload of ContinueWith that accepts a state object (used in IAsyncResult).
+			// Such overloads have been added in .NET 4.5, so this can be removed if/when the project is promoted to that version.
+			// An example of such an added overload can be found at: http://msdn.microsoft.com/en-us/library/hh160386.aspx
 
-     var completionSource = new TaskCompletionSource<TResponse>(state);
+			var completionSource = new TaskCompletionSource<TResponse>(state);
 
-     QueryGoogleAPIAsync(request).ContinueWith(t =>
-     {
-       if (t.IsFaulted)
-         completionSource.SetException(t.Exception.InnerException);
-       else if (t.IsCanceled)
-         completionSource.SetCanceled();
-       else
-         completionSource.SetResult(t.Result);
+			QueryGoogleAPIAsync(request).ContinueWith(t =>
+			{
+				if (t.IsFaulted)
+					completionSource.SetException(t.Exception.InnerException);
+				else if (t.IsCanceled)
+					completionSource.SetCanceled();
+				else
+					completionSource.SetResult(t.Result);
 
-       asyncCallback(completionSource.Task);
-     }, TaskContinuationOptions.ExecuteSynchronously);
+				asyncCallback(completionSource.Task);
+			}, TaskContinuationOptions.ExecuteSynchronously);
 
-     return completionSource.Task;
-   }
+			return completionSource.Task;
+		}
 
-   protected TResponse EndQueryGoogleAPI(IAsyncResult asyncResult)
-   {
-     try
-     {
-       return ((Task<TResponse>)asyncResult).Result;
-     }
-     catch (AggregateException ex)
-     {
-       throw ex.InnerException;
-     }
-   }
+		protected TResponse EndQueryGoogleAPI(IAsyncResult asyncResult)
+		{
+			return ((Task<TResponse>)asyncResult).Result;
+		}
 
-   protected TResponse QueryGoogleAPI(TRequest request)
-   {
-     try
-     {
-       return QueryGoogleAPIAsync(request).Result;
-     }
-     catch (AggregateException ex)
-     {
-       throw ex.InnerException;
-     }  
-   
- }
+		protected TResponse QueryGoogleAPI(TRequest request)
+		{
+			return QueryGoogleAPIAsync(request).Result;
+		}
 
-   protected Task<TResponse> QueryGoogleAPIAsync(TRequest request)
-   {
-     TaskCompletionSource<TResponse> tcs = new TaskCompletionSource<TResponse>();
+		protected Task<TResponse> QueryGoogleAPIAsync(TRequest request)
+		{
+			TaskCompletionSource<TResponse> tcs = new TaskCompletionSource<TResponse>();
 
-     Task.Factory.StartNew(() =>
-                             {
-                               WebClient client;
-                               Uri uri;
+			Task.Factory.StartNew(() =>
+															{
+																WebClient client;
+																Uri uri;
 
-                               try
-                               {
-                                 client = new WebClient();
-                                 ConfigureUnderlyingWebClient(client, request);
-                                 uri = GetUri(request);
-                               }
-                               catch (Exception ex)
-                               {
-                                 tcs.SetException(ex);
-                                 return;
-                               }
+																try
+																{
+																	client = new WebClient();
+																	ConfigureUnderlyingWebClient(client, request);
+																	uri = GetUri(request);
+																}
+																catch (Exception ex)
+																{
+																	tcs.SetException(ex);
+																	return;
+																}
 
-                               client.OpenReadStreamAsync(uri)
-                                 .ContinueWith(t =>
-                                               {
-                                                 if (!t.IsFaulted)
-                                                 {
-                                                   try
-                                                   {
-                                                     TResponse response = Deserialize(t.Result);
+																client.OpenReadStreamAsync(uri)
+																	.ContinueWith(t =>
+																								{
+																									if (!t.IsFaulted)
+																									{
+																										try
+																										{
+																											TResponse response = Deserialize(t.Result);
 
-                                                     tcs.SetResult(response);
-                                                   }
-                                                   catch (Exception ex)
-                                                   {
-                                                     tcs.SetException(ex);
-                                                   }
-                                                 }
-                                                 else
-                                                 {
-                                                   tcs.SetException(t.Exception.InnerException);
-                                                 }
-                                               }, TaskContinuationOptions.ExecuteSynchronously);
-                             });
+																											tcs.SetResult(response);
+																										}
+																										catch (Exception ex)
+																										{
+																											tcs.SetException(ex);
+																										}
+																									}
+																									else
+																									{
+																										tcs.SetException(t.Exception.InnerException);
+																									}
+																								}, TaskContinuationOptions.ExecuteSynchronously);
+															});
 
-     return tcs.Task;
-   }
+			return tcs.Task;
+		}
 
 
-     Uri GetUri(MapsBaseRequest request)
+		Uri GetUri(MapsBaseRequest request)
 		{
 			string scheme = request.IsSSL ? "https://" : "http://";
 			return new Uri(scheme + BaseUrl + "json");
 		}
-		
+
 		protected abstract void ConfigureUnderlyingWebClient(WebClient wc, MapsBaseRequest request);
 
 		private TResponse Deserialize(Stream stream)
