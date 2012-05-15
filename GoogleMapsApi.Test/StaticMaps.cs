c60fd4fcf685a00b2d65b81135724aef36e7fa7a
Allon-Guralnek <allon.guralnek@gmail.com>
2012-05-15 12:14:50 +0300

    - Fixed "jpg-baselin" to "jpg-baseline" in StaticMapsEngine. - Switched StaticMapsEngine to use QueryStringParametersList. - Made StaticMaps tests use the the AAA pattern (Arrange-Act-Assert).

--- GoogleMapsApi.Test/StaticMaps.cs
+++ GoogleMapsApi.Test/StaticMaps.cs
@@ -13,69 +13,41 @@ namespace GoogleMapsApi.Test
 	[TestFixture]
 	public class StaticMaps
 	{
-		private StaticMapsEngine staticMapGenerator;
-
-		[TestFixtureSetUp]
-		public void Init()
-		{
-			staticMapGenerator = new StaticMapsEngine();
-		}
-
 		[Test(Description = "First basic example")]
 		public void BasicTest()
 		{
 			// downtown New York City
-			StaticMapRequest request = new StaticMapRequest(new AddressLocation("Brooklyn Bridge,New York,NY"), 14, new ImageSize(512, 512))
-																						{
-																							MapType = MapType.Roadmap,
-																							Markers = new List<Marker>(){
-			                                    		                            	new Marker(){
-			                                    		                            	            	Style = new MarkerStyle()
-			                                    		                            	            	        	{
-			                                    		                            	            	        		Color = "blue",
-			                                    		                            	            	        		Label = "S"
+			StaticMapRequest request = new StaticMapRequest(
+				new AddressLocation("Brooklyn Bridge,New York,NY"), 14, new ImageSize(512, 512))
+				{
+					MapType = MapType.Roadmap,
+					Markers =
+						new List<Marker>
+							{
+								new Marker
+									{
+										Style = new MarkerStyle { Color = "blue", Label = "S" },
+										Locations = new List<ILocation> { new Location(40.702147, -74.015794) }
+									},
+								new Marker
+									{
+										Style = new MarkerStyle { Color = "green", Label = "G" },
+										Locations = new List<ILocation> { new Location(40.711614, -74.012318) }
+									},
+								new Marker
+									{
+										Style = new MarkerStyle { Color = "red", Label = "C" },
+										Locations = new List<ILocation> { new Location(40.718217, -73.998284) }
+									}
+							},
+					Sensor = false
+				};
+			string expectedResult = "http://maps.google.com/maps/api/staticmap" +
+									"?center=Brooklyn%20Bridge%2CNew%20York%2CNY&zoom=14&size=512x512&maptype=roadmap" +
+									"&markers=color%3Ablue%7Clabel%3AS%7C40.702147%2C-74.015794&markers=color%3Agreen%7Clabel%3AG%7C40.711614%2C-74.012318" +
+									"&markers=color%3Ared%7Clabel%3AC%7C40.718217%2C-73.998284&sensor=false";
 
-			                                    		                            	            	        	},
-			                                    		                            	            	Locations = new List<ILocation>()
-			                                    		                            	            	            	{
-			                                    		                            	            	            		new Location(40.702147,-74.015794)
-			                                    		                            	            	            	}
-			                                    		                            	            },
-																																												new Marker()
-																																													{
-																																														Style = new MarkerStyle()
-																																														        	{
-																																														        		Color = "green",
-																																																				Label = "G"
-																																														        	},
-																																																			Locations = new List<ILocation>()
-																																																			            	{
-																																																			            		new Location(40.711614,-74.012318)
-																																																			            	}
-																																													},
-			                                    		                            	new Marker()
-			                                    		                            		{
-			                                    		                            			Style = new MarkerStyle()
-			                                    		                            			        	{
-			                                    		                            			        		Color = "red",
-			                                    		                            			        		Label = "C"
-			                                    		                            			        	},
-			                                    		                            			Locations = new List<ILocation>()
-			                                    		                            			            	{
-			                                    		                            			            		new Location(40.718217,-73.998284)
-			                                    		                            			            	}
-																		
-			                                    		                            		}
-			                                    		                            },
-																							Sensor = false
-
-																						};
-
-			string generateStaticMapURL = staticMapGenerator.GenerateStaticMapURL(request);
-
-
-			string expectedResult =
-        @"http://maps.google.com/maps/api/staticmap?center=Brooklyn%20Bridge%2CNew%20York%2CNY&zoom=14&size=512x512&maptype=roadmap&markers=color%3Ablue%7Clabel%3AS%7C40.702147%2C-74.015794&markers=color%3Agreen%7Clabel%3AG%7C40.711614%2C-74.012318&markers=color%3Ared%7Clabel%3AC%7C40.718217%2C-73.998284&sensor=false";
+			string generateStaticMapURL = new StaticMapsEngine().GenerateStaticMapURL(request);
 
 			Assert.AreEqual(expectedResult, generateStaticMapURL);
 		}
@@ -83,12 +55,11 @@ namespace GoogleMapsApi.Test
 		[Test]
 		public void AddressTest()
 		{
-			StaticMapRequest request = new StaticMapRequest(new AddressLocation("Berkeley,CA"), 14, new ImageSize(400, 400));
+			var request = new StaticMapRequest(new AddressLocation("Berkeley,CA"), 14, new ImageSize(400, 400));
+			string expectedResult = "http://maps.google.com/maps/api/staticmap" +
+									"?center=Berkeley%2CCA&zoom=14&size=400x400&sensor=false";
 
-			string generateStaticMapURL = staticMapGenerator.GenerateStaticMapURL(request);
-
-			string expectedResult =
-        @"http://maps.google.com/maps/api/staticmap?center=Berkeley%2CCA&zoom=14&size=400x400&sensor=false";
+			string generateStaticMapURL = new StaticMapsEngine().GenerateStaticMapURL(request);
 
 			Assert.AreEqual(expectedResult, generateStaticMapURL);
 		}
@@ -96,12 +67,11 @@ namespace GoogleMapsApi.Test
 		[Test]
 		public void ZoomLevels()
 		{
-			StaticMapRequest request = new StaticMapRequest(new Location(40.714728, -73.998672), 12, new ImageSize(400, 400));
+			var request = new StaticMapRequest(new Location(40.714728, -73.998672), 12, new ImageSize(400, 400));
+			string expectedResult = "http://maps.google.com/maps/api/staticmap" +
+									"?center=40.714728%2C-73.998672&zoom=12&size=400x400&sensor=false";
 
-			string generateStaticMapURL = staticMapGenerator.GenerateStaticMapURL(request);
-
-			string expectedResult =
-				@"http://maps.google.com/maps/api/staticmap?center=40.714728%2C-73.998672&zoom=12&size=400x400&sensor=false";
+			string generateStaticMapURL = new StaticMapsEngine().GenerateStaticMapURL(request);
 
 			Assert.AreEqual(expectedResult, generateStaticMapURL);
 		}
@@ -109,12 +79,11 @@ namespace GoogleMapsApi.Test
 		[Test]
 		public void ImageSize()
 		{
-			StaticMapRequest request = new StaticMapRequest(new Location(0, 0), 1, new ImageSize(400, 50));
+			var request = new StaticMapRequest(new Location(0, 0), 1, new ImageSize(400, 50));
+			string expectedResult = "http://maps.google.com/maps/api/staticmap" +
+									"?center=0%2C0&zoom=1&size=400x50&sensor=false";
 
-			string generateStaticMapURL = staticMapGenerator.GenerateStaticMapURL(request);
-
-			string expectedResult =
-				@"http://maps.google.com/maps/api/staticmap?center=0%2C0&zoom=1&size=400x50&sensor=false";
+			string generateStaticMapURL = new StaticMapsEngine().GenerateStaticMapURL(request);
 
 			Assert.AreEqual(expectedResult, generateStaticMapURL);
 		}
@@ -123,18 +92,14 @@ namespace GoogleMapsApi.Test
 		[Test]
 		public void MapTypes()
 		{
-			StaticMapRequest request = new StaticMapRequest(new Location(40.714728, -73.998672), 12, new ImageSize(400, 400));
-
+			var request = new StaticMapRequest(new Location(40.714728, -73.998672), 12, new ImageSize(400, 400));
 			request.MapType = MapType.Terrain;
+			string expectedResult = @"http://maps.google.com/maps/api/staticmap" +
+									@"?center=40.714728%2C-73.998672&zoom=12&size=400x400&maptype=terrain&sensor=false";
 
-			string generateStaticMapURL = staticMapGenerator.GenerateStaticMapURL(request);
+			string actualResult = new StaticMapsEngine().GenerateStaticMapURL(request);
 
-			string expectedResult =
-				@"http://maps.google.com/maps/api/staticmap?center=40.714728%2C-73.998672&zoom=12&size=400x400&maptype=terrain&sensor=false";
-
-			Assert.AreEqual(expectedResult, generateStaticMapURL);
+			Assert.AreEqual(expectedResult, actualResult);
 		}
-
-
 	}
 }
