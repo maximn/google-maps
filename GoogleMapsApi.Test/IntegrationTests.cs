3051976d46fca1a1ca44103a00bf3c387cff58ec
Maxim Novak <maxim.novak@gmail.com>
2012-07-21 17:56:46 +0300

    spaces->tabs

--- GoogleMapsApi.Test/IntegrationTests.cs
+++ GoogleMapsApi.Test/IntegrationTests.cs
@@ -16,19 +16,19 @@ using NUnit.Framework;
 
 /////////////////////////////////////////////////////////////////////////
 //
-//  Warning: The tests below run against the real Google API web
-//           servers and count towards your query limit. Also, the tests
-//           require a working internet connection in order to pass.
-//           Their run time may vary depending on your connection,
-//           network congestion and the current load on Google's servers.
+//  Warning:	The tests below run against the real Google API web
+//				servers and count towards your query limit. Also, the tests
+//				require a working internet connection in order to pass.
+//				Their run time may vary depending on your connection,
+//				network congestion and the current load on Google's servers.
 //
 /////////////////////////////////////////////////////////////////////////
 
 
 /////////////////////////////////////////////////////////////////////////
 //
-//  Note: Tests are written in the Arrange-Act-Assert pattern, where each
-//        section is separated by a blank line.
+//  Note:	Tests are written in the Arrange-Act-Assert pattern, where each
+//			section is separated by a blank line.
 //
 /////////////////////////////////////////////////////////////////////////
 
