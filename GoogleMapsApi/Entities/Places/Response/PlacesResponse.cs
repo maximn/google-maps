98af2eadef0a4d27a53161767f210ebe2aa51d27
Shane Menchions <shane (at) prospectinteractive.com>
2014-05-14 13:57:59 -0700

    Enable pagination for Places requests and responses

--- GoogleMapsApi/Entities/Places/Response/PlacesResponse.cs
+++ GoogleMapsApi/Entities/Places/Response/PlacesResponse.cs
@@ -27,6 +27,17 @@ namespace GoogleMapsApi.Entities.Places.Response
 			}
 		}
 
+        /// <summary>
+        /// If there is a next page of results, the token will be supplied by Google. Token should be set on the next PlacesRequest object to get the next page of results from Google.
+        /// If null is returned, there is no next page of results.
+        /// </summary>
+        [DataMember(Name = "next_page_token")]
+        public string NextPage
+        {
+            get;
+            set;
+        }
+
 		/// <summary>
 		/// "results" contains an array of places, with information about the place. See Place Search Results for information about these results. The Places API returns up to 20 establishment results. Additionally, political results may be returned which serve to identify the area of the request.
 		/// </summary>
