3b666596d0706c31919a36f4de41bc7f7103310c
George Shaw <george.shaw@autocab.com>
2014-12-02 16:02:02 +0000

    Add PlaceId for Place Autocomplete API.

--- GoogleMapsApi/Entities/PlaceAutocomplete/Response/Prediction.cs
+++ GoogleMapsApi/Entities/PlaceAutocomplete/Response/Prediction.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.Serialization;
+﻿using System;
+using System.Runtime.Serialization;
 using GoogleMapsApi.Entities.Common;
 
 namespace GoogleMapsApi.Entities.PlaceAutocomplete.Response
@@ -16,20 +17,29 @@ namespace GoogleMapsApi.Entities.PlaceAutocomplete.Response
 		[DataMember(Name = "description")]
 		public string Description { get; set; }
 
-        /// <summary>
+		/// <summary>
+		/// Contains a unique identifier for this place. To retrieve information about this place, pass this identifier in the placeId field of
+		/// a Places API request.
+		/// </summary>
+		[DataMember( Name = "place_id" )]
+		public string PlaceId { get; set; }
+		
+		/// <summary>
         /// Contains a unique token that you can use to retrieve additional information about this place in a Place Details request. 
         /// You can store this token and use it at any time in future to refresh cached data about this place, but the same token is 
         /// not guaranteed to be returned for any given place across different searches.
         /// </summary>
         [DataMember(Name = "reference")]
-        public string Reference { get; set; }
+		[Obsolete( "Use place_id instead.  See https://developers.google.com/places/documentation/search#deprecation for more information." )]
+		public string Reference { get; set; }
 
         /// <summary>
         /// Contains a unique stable identifier denoting this place. This identifier may not be used to retrieve information about this
         /// place, but can be used to consolidate data about this place, and to verify the identity of a place across separate searches.
         /// </summary>
         [DataMember(Name = "id")]
-        public string ID { get; set; }
+		[Obsolete( "Use place_id instead.  See https://developers.google.com/places/documentation/search#deprecation for more information." )]
+		public string ID { get; set; }
 
         /// <summary>
         /// Contains an array of terms identifying each section of the returned description (a section of the description is generally 
@@ -49,7 +59,7 @@ namespace GoogleMapsApi.Entities.PlaceAutocomplete.Response
         /// Contains an offset value and a length. These describe the location of the entered term in the prediction result text, so that 
         /// the term can be highlighted if desired.
         /// </summary>
-		[DataMember( Name = "matched_substrings" )]
+		[DataMember(Name = "matched_substrings")]
 		public MatchedSubstring[] MatchedSubstrings { get; set; }
 	}
 }
