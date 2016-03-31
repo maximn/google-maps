709632a440b045cfb973b6e0972c83a8cb33a582
mklimmasch <m.klimmasch@googlemail.com>
2016-03-31 09:22:14 +0200

    Fixed UNIX timestamp DST issue

--- GoogleMapsApi/Engine/UnixTimeConverter.cs
+++ GoogleMapsApi/Engine/UnixTimeConverter.cs
@@ -4,14 +4,14 @@ namespace GoogleMapsApi.Engine
 {
 	public static class UnixTimeConverter
 	{
-		private static DateTime epoch = new DateTime(1970, 1, 1);
+		private static DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
 
 		/// <summary>
 		/// Converts a DateTime to a Unix timestamp
 		/// </summary>
 		public static int DateTimeToUnixTimestamp(DateTime dateTime)
 		{
-			return (int)(dateTime - epoch.ToLocalTime()).TotalSeconds;
+			return (int)(dateTime.ToUniversalTime() - epoch).TotalSeconds;
 		}
 	}
 }
