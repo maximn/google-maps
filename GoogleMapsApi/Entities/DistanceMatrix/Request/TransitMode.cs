22ddf48576199d47b0e3b03531caf2a1fcc8b0f3
Chris Smith <chris.smith.dev@gmail.com>
2016-08-22 13:00:20 +0100

    Added new enums for DataMatrix Request optional parameters

---
+++ GoogleMapsApi/Entities/DistanceMatrix/Request/TransitMode.cs
@@ -0,0 +1,20 @@
+﻿namespace GoogleMapsApi.Entities.DistanceMatrix.Request
+{
+    using System.Runtime.Serialization;
+
+    [DataContract]
+    public enum DistanceMatrixTransitModes
+    {
+        // To be used for 'transit' travel mode only
+        [EnumMember]
+        bus, // route should prefer travel by bus
+        [EnumMember]
+        subway, // route should prefer travel by subway
+        [EnumMember]
+        train, // route should prefer travel by train
+        [EnumMember]
+        tram, // route should prefer travel by tram or light rail
+        [EnumMember]
+        rail, // route should prefer travel by train, tram, light rail and subway
+    }
+}
---
+++ GoogleMapsApi/Entities/DistanceMatrix/Request/UnitSystem.cs
@@ -0,0 +1,13 @@
+﻿namespace GoogleMapsApi.Entities.DistanceMatrix.Request
+{
+    using System.Runtime.Serialization;
+
+    [DataContract]
+    public enum DistanceMatrixUnitSystems
+    {
+        [EnumMember]
+        metric, // kilometers an meters.
+        [EnumMember]
+        imperial, // miles and feet.
+    }
+}
