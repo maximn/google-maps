f9146a6f8ae3a6bc94a9ffbd404eb3bcd402718f
jwkerley <joe.kerley@2b-or-not.co.uk>
2016-11-08 09:57:25 +0000

    Fix Incorrect LocationString Longitude Near Zero
    Ensure we reliably convert lat/long to non-scientific notation -
    previously 0.000009 was converted to "9E-06" rather than "0.000009".

---
+++ GoogleMapsApi.Test/LocationToStringTest.cs
@@ -0,0 +1,17 @@
+﻿using GoogleMapsApi.Entities.Common;
+using NUnit.Framework;
+
+namespace GoogleMapsApi.Test
+{
+    [TestFixture]
+    public class LocationToStringTest
+    {
+        [Test]
+        public void WhenNearZeroLongitude_ExpectCorrectToString()
+        {
+            // Longitude of 0.000009 is converted to 9E-06 using Invariant ToString, but we need 0.000009
+            var location = new Location(57.231d, 0.000009d);
+            Assert.AreEqual("57.231,0.000009", location.ToString());
+        }
+    }
+}
