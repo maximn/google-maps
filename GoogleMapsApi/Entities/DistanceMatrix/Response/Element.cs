ff9c3beb003cf61d106e7d969c2254def9cf3f89
kzudin <kzudin@yandex.ru>
2017-06-05 21:01:57 +0300

    Fixed spaces.

--- GoogleMapsApi/Entities/DistanceMatrix/Response/Element.cs
+++ GoogleMapsApi/Entities/DistanceMatrix/Response/Element.cs
@@ -1,46 +1,51 @@
 ﻿namespace GoogleMapsApi.Entities.DistanceMatrix.Response
 {
-	using System;
-	using System.Runtime.Serialization;
+    using System;
+    using System.Runtime.Serialization;
 
-	[DataContract(Name = "element")]
-	public class Element
-	{
-		[DataMember(Name = "status")]
-		public string StatusStr
-		{
-			get
-			{
-				return Status.ToString();
-			}
-			set
-			{
-				Status = (DistanceMatrixElementStatusCodes)Enum.Parse(typeof(DistanceMatrixElementStatusCodes), value);
-			}
-		}
+    using GoogleMapsApi.Entities.Directions.Response;
 
-		/// <summary>
-		/// "status" See Status Codes for a list of possible status codes.
-		/// </summary>
-		public DistanceMatrixElementStatusCodes Status { get; set; }
+    [DataContract(Name = "element")]
+    public class Element
+    {
+        [DataMember(Name = "status")]
+        public string StatusStr
+        {
+            get
+            {
+                return Status.ToString();
+            }
+            set
+            {
+                Status = (DistanceMatrixElementStatusCodes)Enum.Parse(typeof(DistanceMatrixElementStatusCodes), value);
+            }
+        }
 
-		/// <summary>
+        /// <summary>
+        /// "status" See Status Codes for a list of possible status codes.
+        /// </summary>
+        public DistanceMatrixElementStatusCodes Status { get; set; }
+
+        /// <summary>
 		///  distance: The total distance of this route, expressed in meters (value) and as text
 		/// </summary>
 		[DataMember(Name = "distance")]
-		public Distance Distance { get; set; }
+        public Distance Distance { get; set; }
 
-		/// <summary>
-		/// duration: The length of time it takes to travel this route
-		/// </summary>
-		[DataMember(Name = "duration")]
-		public Duration Duration { get; set; }
+        /// <summary>
+        /// duration: The length of time it takes to travel this route
+        /// </summary>
+        [DataMember(Name = "duration")]
+        public Duration Duration { get; set; }
 
-		/// <summary>
+        /// <summary>
 		/// duration_in_traffic The length of time it takes to travel this route, based on current and historical traffic conditions. 
 		/// See the traffic_model request parameter for the options you can use to request that the returned value is optimistic, pessimistic, or a best-guess estimate.
 		/// </summary>
 		[DataMember(Name = "duration_in_traffic")]
-		public Duration DurationInTraffic { get; set; }
-	}
+        public Duration DurationInTraffic { get; set; }
+
+
+
+    }
 }
