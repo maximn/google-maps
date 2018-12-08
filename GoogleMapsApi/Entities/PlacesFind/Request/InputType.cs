e48ae479b87bc6b6c6d9c6897b6b47842f759bb2
Evan <rossiter10@users.noreply.github.com>
2018-12-08 15:59:14 -0600

    added Find Place API call

---
+++ GoogleMapsApi/Entities/PlacesFind/Request/InputType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleMapsApi.Entities.PlacesFind.Request
+{
+    public enum InputType
+    {
+		[EnumMember(Value = "textquery")]
+        TextQuery,
+		[EnumMember(Value = "phonenumber")]
+        PhoneNumber
+    }
+}
---
+++ GoogleMapsApi/Entities/PlacesFind/Response/Candidate.cs
@@ -0,0 +1,62 @@
+﻿using GoogleMapsApi.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleMapsApi.Entities.PlacesFind.Response
+{
+    [DataContract]
+    public class Candidate
+    {
+        // basic fields
+        [DataMember(Name = "formatted_address")]
+        public string FormattedAddress { get; set; }
+
+        [DataMember(Name = "geometry")]
+        public Geometry Geometry { get; set; }
+
+        [DataMember(Name = "icon")]
+        public string Icon { get; set; }
+
+        [DataMember(Name = "id")]
+        public string ID { get; set; }
+
+        [DataMember(Name = "name")]
+        public string Name { get; set; }
+
+        [DataMember(Name = "permanently_closed")]
+        public bool? PermanentlyClosed { get; set; }
+
+        [DataMember(Name = "photos")]
+        public IEnumerable<Photo> Photos { get; set; }
+
+        [DataMember(Name = "place_id")]
+        public string PlaceId { get; set; }
+
+        [DataMember(Name = "plus_code")]
+        public string PlusCode { get; set; }
+
+        [DataMember(Name = "scope")]
+        public string Scope { get; set; }
+
+        [DataMember(Name = "types")]
+        public string[] Types { get; set; }
+
+        // contact fields
+        /// <summary>
+        /// Place Search returns only open_now; use a Place Details request to get the full opening_hours results.
+        /// </summary>
+        [DataMember(Name = "opening_hours")]
+        public OpeningHours OpeningHours { get; set; }
+
+        // atmosphere fields
+        [DataMember(Name = "price_level")]
+        public int? PriceLevel { get; set; }
+
+        [DataMember(Name = "rating")]
+        public double? Rating { get; set; }
+    }
+}
