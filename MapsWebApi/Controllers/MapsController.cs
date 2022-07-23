905706748af867319b9d5ba1a75cafe4d07bd608
molod01 <manuylovnikitos@gmail.com>
2022-07-23 11:55:01 +0300

    final

--- MapsWebApi/Controllers/MapsController.cs
+++ MapsWebApi/Controllers/MapsController.cs
@@ -6,56 +6,81 @@ using GoogleMapsApi.Entities.Geocoding.Request;
 using GoogleMapsApi.Entities.Geocoding.Response;
 using GoogleMapsApi.StaticMaps;
 using GoogleMapsApi.StaticMaps.Entities;
+using GoogleMapsApi.StaticMaps.Enums;
+using MapsWebApi.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Web.Helpers;
 
 namespace MapsWebApi.Controllers
 {
+    public class Data
+    {
+        public string img;
+        public string start;
+        public string end;
+        public string duration;
+        public Data(string img, string start, string end, string duration)
+        {
+            this.img = img;
+            this.start = start;
+            this.end = end;
+            this.duration = duration;
+            
+        }
+    }
+
     [Route("api/[controller]")]
     [ApiController]
     public class MapsController : ControllerBase
     {
-        [HttpGet]
-        public IActionResult GetMapImage(string firstAddress, string secondAddress)
+        IConfiguration configuration;
+        public MapsController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        [HttpPost]
+        public IActionResult GetMapImage(CustomImageRequest request)
         {
             DirectionsRequest directionsRequest = new DirectionsRequest()
             {
-                ApiKey = "AIzaSyDikeBAymgSWrWz-9Y7Danr2mNewZV_MwI",
-                Origin = firstAddress,
-                Destination = secondAddress
+                ApiKey = configuration["Key"],
+                Origin = $"{request.City}, {request.Origin}",
+                Destination = $"{request.City}, {request.Destination}"
             };
 
             DirectionsResponse directions = GoogleMaps.Directions.QueryAsync(directionsRequest).Result;
-            Console.WriteLine(directions);
-
-            //GeocodingRequest geocodeRequest = new GeocodingRequest()
-            //{
-            //    ApiKey = "AIzaSyDikeBAymgSWrWz-9Y7Danr2mNewZV_MwI",
-            //    Address = "odessa",
-            //};
-            //var geocodingEngine = GoogleMaps.Geocode;
-            //GeocodingResponse geocode = geocodingEngine.QueryAsync(geocodeRequest).Result;
-            //Console.WriteLine(geocode);
 
             StaticMapsEngine staticMapGenerator = new StaticMapsEngine();
 
+
             IEnumerable<Step> steps = directions.Routes.First().Legs.First().Steps;
-            IList<ILocationString> path = steps.Select(step => step.StartLocation).ToList<ILocationString>();
+            IList<ILocationString> path = steps.Select(step => step.StartLocation)
+                                               .ToList<ILocationString>();
             path.Add(steps.Last().EndLocation);
 
-            string url = staticMapGenerator.GenerateStaticMapURL(new StaticMapRequest(null, 12, new ImageSize(800, 400))
+            StaticMapRequest staticMapRequest = new StaticMapRequest(null, default, new ImageSize(1000, 600))
             {
-                ApiKey = "AIzaSyDikeBAymgSWrWz-9Y7Danr2mNewZV_MwI",
-                Pathes = new List<GoogleMapsApi.StaticMaps.Entities.Path>(){ new GoogleMapsApi.StaticMaps.Entities.Path()
+                ApiKey = configuration["Key"],
+                Pathes = new List<GoogleMapsApi.StaticMaps.Entities.Path>()
                 {
+                    new GoogleMapsApi.StaticMaps.Entities.Path()
+                    {
                         Style = new PathStyle()
                         {
-                                Color = "red"
+                            Color = "0x800080",
+                            Weight = 4
                         },
                         Locations = path
-                }}
-            });
+                    }
+                },
+                MapType = request.MapType
+            };
 
-            return Ok(url);
+            string url = staticMapGenerator.GenerateStaticMapURL(staticMapRequest);
+
+            var route = directions.Routes.First().Legs.First();
+            return Ok(JsonConvert.SerializeObject(new Data(url, route.StartAddress, route.EndAddress, route.Duration.Text)));
         }
     }
 }
--- MapsWebApi/Program.cs
+++ MapsWebApi/Program.cs
@@ -3,6 +3,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Configuration.AddJsonFile("appsettings.json");
+ConfigurationManager configuration = builder.Configuration;
+
 
 var app = builder.Build();
 
@@ -10,6 +13,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
 }
 
 app.UseHttpsRedirection();
