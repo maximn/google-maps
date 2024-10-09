03e8fea8cc13cda9f7c4a888dd38c915e23077ba
Max Novak <maxim.novak@gmail.com>
2024-10-09 19:13:56 +0300

    Fix warning - Exception shouldn't be serializable

--- GoogleMapsApi/Entities/Directions/Response/PointsDecodingException.cs
+++ GoogleMapsApi/Entities/Directions/Response/PointsDecodingException.cs
@@ -1,18 +1,9 @@
 ﻿using System;
-using System.Runtime.Serialization;
 
 namespace GoogleMapsApi.Entities.Directions.Response
 {
-	[Serializable]
 	public class PointsDecodingException : Exception
 	{
-		//
-		// For guidelines regarding the creation of new exception types, see
-		//    http://msdn.microsoft.com/library/default.asp?url=/library/en-us/cpgenref/html/cpconerrorraisinghandlingguidelines.asp
-		// and
-		//    http://msdn.microsoft.com/library/default.asp?url=/library/en-us/dncscol/html/csharp07192001.asp
-		//
-
 		public string EncodedString { get; set; }
 
 		public PointsDecodingException()
@@ -26,12 +17,6 @@ namespace GoogleMapsApi.Entities.Directions.Response
 		public PointsDecodingException(string message, string encodedString, Exception inner) : base(message, inner)
 		{
 			EncodedString = encodedString;
-		}
-
-		protected PointsDecodingException(
-			SerializationInfo info,
-			StreamingContext context) : base(info, context)
-		{
 		}
 	}
 }
