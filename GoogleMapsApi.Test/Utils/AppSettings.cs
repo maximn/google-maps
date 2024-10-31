abb3cd2759c8356eecde761952d2f9304b56bb92
Henrik Sozzi <henrik.sozzi@gmail.com>
2024-10-31 19:07:30 +0100

    Updated dependencies and now compatible from netstandard2.0 to net8.0
    I've updated all dependencies and reverded the base target from netstandard2.1 (introduced in ver 1.2.7) to netstandard2.0 as this keeps it compatible with the widely used Net Framework 4.8.
    Added net4.8 to tests and adapted appsettings.json code to work on every platform.
    All tests passed on all targets.

---
+++ GoogleMapsApi.Test/Utils/AppSettings.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleMapsApi.Test.Utils
+{
+    internal class AppSettings
+    {
+        [JsonProperty(PropertyName ="GOOGLE_API_KEY")]
+        public string? GoogleApiKey { get; set; }
+
+        public static AppSettings? Load()
+        {
+            var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "appsettings.json");
+            if (!File.Exists(path)) return null;
+            return JsonConvert.DeserializeObject<AppSettings>(File.ReadAllText(path));
+        }
+    }
+}
