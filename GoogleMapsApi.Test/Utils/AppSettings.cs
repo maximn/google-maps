150049b6038b9b2db60fbffaf9159b834301ff50
Maxim Novak <maxim.novak@gmail.com>
2025-09-03 21:21:00 +0100

    refactor:intorduce System.Text.Json, partially apply (#179)

--- GoogleMapsApi.Test/Utils/AppSettings.cs
+++ GoogleMapsApi.Test/Utils/AppSettings.cs
@@ -1,22 +1,24 @@
-﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace GoogleMapsApi.Test.Utils
 {
     internal class AppSettings
     {
-        [JsonProperty(PropertyName ="GOOGLE_API_KEY")]
+        [JsonPropertyName("GOOGLE_API_KEY")]
         public string? GoogleApiKey { get; set; }
 
         public static AppSettings? Load()
         {
             var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "appsettings.json");
             if (!File.Exists(path)) return null;
-            return JsonConvert.DeserializeObject<AppSettings>(File.ReadAllText(path));
+            return JsonSerializer.Deserialize<AppSettings>(File.ReadAllText(path));
         }
     }
 }
